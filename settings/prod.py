136a5d07e4825094c813ecf463bde20d449b1673
Adam Wester <awwester@gmail.com>
2016-09-21 22:59:56 -0500

    use postgres on aws eb

--- settings/prod.py
+++ settings/prod.py
@@ -3,6 +3,18 @@ from .dev import *
 
 ALLOWED_HOSTS = ['*']
 
+# RDS variables come automatically from AWS RDS
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
+
 # STATIC FILE SETTINGS
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
